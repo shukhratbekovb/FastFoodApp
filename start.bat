a05ad81a1b1d6f23f927bae6b37a926e5de6324d
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-11-29 19:54:04 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,14 +1,12 @@
 @echo off
 
-REM --- Запуск Docker Desktop ---
-net start com.docker.service >nul 2>&1
-
-REM --- Ожидание старта Docker Engine ---
-:wait
+REM --- Ожидание запуска Rancher Desktop Docker Engine ---
+echo Waiting for Docker Engine from Rancher Desktop...
+:check
 docker info >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     timeout /t 3 >nul
-    goto wait
+    goto check
 )
 
 REM --- Перезапуск контейнеров ---
@@ -18,14 +16,13 @@ docker-compose up --build -d
 REM --- Ожидание перед открытием браузера ---
 timeout /t 6 >nul
 
+REM --- Открытие Chrome ---
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:3000/catalog
 
-REM --- Открытие браузера ---
-start  http://localhost:3000/catalog
-
-REM Подождать 2 секунды пока окно откроется
+REM --- Подождать чтобы окно появилось ---
 timeout /t 2 >nul
 
-REM Отправить F11 в активное окно
+REM --- Авто-нажатие F11 ---
 powershell -command "(new-object -com wscript.shell).SendKeys('{F11}')"
 
 exit
