b10df97bee0c33c1b19aa884803f9cd8e0d526e0
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-11-29 19:30:13 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -18,7 +18,14 @@ docker-compose up --build -d
 REM --- Ожидание перед открытием браузера ---
 timeout /t 6 >nul
 
+
 REM --- Открытие браузера ---
-start http://localhost:3000/catalog
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:3000/catalog
+
+REM Подождать 2 секунды пока окно откроется
+timeout /t 2 >nul
+
+REM Отправить F11 в активное окно
+powershell -command "(new-object -com wscript.shell).SendKeys('{F11}')"
 
 exit
