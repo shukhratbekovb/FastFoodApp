5f90d66d0fb2a3ef2696937088ad8e810f4ddcbf
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-11-30 14:37:40 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ from printer import print_receipt
 
 app = FastAPI()
 IP = "192.168.0.50"
-p = Network(IP)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,14 +24,18 @@ app.add_middleware(
 async def print_check(
         request: PrintSchema
 ):
+    p = Network(IP, timeout=10)
     print_receipt(p, request, "client")
     print_receipt(p, request, "kitchen")
+    p.close()
 
 @app.post("/print/report")
 async def print_report(
         request: LargePrintSchema
 ):
+    p = Network(IP, timeout=10)
     print_receipt(p, request, "internal")
+    p.close()
 
 
 if __name__ == "__main__":
